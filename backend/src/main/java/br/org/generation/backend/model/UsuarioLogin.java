e195ac855aaacf5223d408bc1a7c3f2ed3e76132
AmandaFaria <amanda34faria@gmail.com>
2021-11-17 10:35:35 -0300

    Adicionando o Usuário Login

---
+++ backend/src/main/java/br/org/generation/backend/model/UsuarioLogin.java
@@ -0,0 +1,44 @@
+package br.org.generation.backend.model;
+
+public class UsuarioLogin {
+
+	private long id;
+	
+	private String usuario;
+	
+	private String senha;
+	
+	private String token;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
