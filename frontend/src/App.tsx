585689977e0cd7c4814d851e9e26d15df1d4b20b
Isabella Marinho <isabellamarcelomarinho@gmail.com>
2021-12-06 18:50:00 -0300

    Integração com a API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import CadastroUsuario from './paginas/cadastro/Cadastro';
+import CadastroUsuario from './paginas/cadastro/CadastroUsuario';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import './App.css';
