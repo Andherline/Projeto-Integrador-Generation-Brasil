203272d118fce9b0583352af9694595d70e4d5a0
Andherline <sciosubitis@gmail.com>
2021-12-19 12:56:52 -0300

    listar produtos em card.

--- frontend/src/components/produtos/listaProduto/ListaProduto.tsx
+++ frontend/src/components/produtos/listaProduto/ListaProduto.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Produto from '../../../models/Produto';
 import { busca } from '../../../services/Service'
-import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import { Box, Card, CardActions, CardContent, Button, Typography, Grid } from '@material-ui/core';
 import './ListaProduto.css';
 import { useHistory } from 'react-router-dom'
 import { useSelector } from 'react-redux';
@@ -38,60 +38,48 @@ function ListaProduto() {
 
     return (
         <>
-            {
-                produtos.map(produto => (
-                    <Box m={2} >
-                        <Card variant="outlined">
-                            <CardContent>
+            <Box display="flex" justifyContent="center" flexWrap="wrap">
+                {
+                    produtos.map(produto => (
+                        <Box border={1} marginY={5} marginX={2} padding={2}
+                            className="card-produto">
+                            <Box className='caixa-produto' display='flex' justifyContent='center'>
+                                <img src={produto.foto} width="100%" alt="" className='img-produto' />
+                            </Box>
+                            <Box justifyContent='center'>
+                                <Typography variant="h5" color="initial" align='center'>{produto.nome}</Typography>
+                                <Typography variant="h5" color="initial" align='center'>{produto.descricao}</Typography>
+                                <Typography variant="h5" color="initial" align='center'>R$ {produto.preco}</Typography>
+                                <Typography variant="h5" color="initial" align='center'>{produto.categoria?.palavrachave}</Typography>
+                            </Box>
+                            <Box display="flex" justifyContent="center" mb={1.5}>
 
-                                <Typography color="textSecondary" gutterBottom>
-                                    Produtos
-                                </Typography>
-                                <img src={produto.foto} alt="" />
-                                <Typography variant="h5" component="h2">
-                                    {produto.nome}
-                                </Typography>
-                                <Typography variant="body2" component="p">
-                                    {produto.preco}
-                                </Typography>
-                                <Typography variant="body2" component="p">
-                                    {produto.descricao}
-                                </Typography>
-
-                                <Typography variant="body2" component="p">
-                                    {produto.categoria?.palavrachave}
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Box display="flex" justifyContent="center" mb={1.5}>
-
-                                    <Link to={`/cadastrarproduto/${produto.id}`} className="text-decorator-none" >
-                                        <Box mx={1}>
-                                            <Button variant="contained" size='small' style={{backgroundColor:'#5F7316', color:'white'}}  >
-                                                atualizar
-                                            </Button>
-                                        </Box>
-                                    </Link>
-                                    <Link to={`/deletarproduto/${produto.id}`} className="text-decorator-none">
-                                        <Box mx={1}>
-                                            <Button variant="contained" size='small' style={{backgroundColor:'white', color:'#5F7316',border:'1px solid #C9E265',borderColor: '#5F7316'}}>
-                                                deletar
-                                            </Button>
-                                        </Box>
-                                    </Link>
-                                    <Link to={`/comprar/${produto.id}`} className="text-decorator-none">
-                                        <Box mx={1}>
-                                            <Button variant="contained" size='small' style={{backgroundColor:'#5F7316', color:'white'}} >
-                                                comprar
-                                            </Button>
-                                        </Box>
-                                    </Link>
-                                </Box>
-                            </CardActions>
-                        </Card>
-                    </Box>
-                ))
-            }
+                                <Link to={`/cadastrarproduto/${produto.id}`} className="text-decorator-none" >
+                                    <Box mx={1}>
+                                        <Button variant="contained" size='small' style={{ backgroundColor: '#5F7316', color: 'white' ,marginTop:'10px'}}>
+                                            atualizar
+                                        </Button>
+                                    </Box>
+                                </Link>
+                                <Link to={`/deletarproduto/${produto.id}`} className="text-decorator-none">
+                                    <Box mx={1}>
+                                        <Button variant="contained" size='small' style={{ backgroundColor: 'white', color: '#5F7316', border: '1px solid #C9E265', borderColor: '#5F7316', marginTop:'10px' }}>
+                                            deletar
+                                        </Button>
+                                    </Box>
+                                </Link>
+                                <Link to={`/comprar/${produto.id}`} className="text-decorator-none">
+                                    <Box mx={1}>
+                                        <Button variant="contained" size='small' style={{ backgroundColor: '#5F7316', color: 'white', marginTop:'10px'}} >
+                                            comprar
+                                        </Button>
+                                    </Box>
+                                </Link>
+                            </Box>
+                        </Box>
+                    ))
+                }
+            </Box>
         </>
     )
 }
